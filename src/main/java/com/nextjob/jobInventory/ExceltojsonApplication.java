9427c2e1688c84f7c842779b6b948268e0e52a22
biswajiit <biswajit.jmishra@gmail.com>
2018-09-01 11:50:32 +0530

    project setup

---
+++ src/main/java/com/nextjob/jobInventory/ExceltojsonApplication.java
@@ -0,0 +1,12 @@
+package com.nextjob.jobInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceltojsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceltojsonApplication.class, args);
+	}
+}
